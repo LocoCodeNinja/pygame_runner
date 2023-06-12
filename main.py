4c268f4151704839d03894b2bd6e07cfa1f88be0
Kai <kitsadal@outlook.com>
2023-06-12 11:36:44 -0600

    jump, random speed snail, collision implemented

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ image_path = BASE_PATH + '/graphics/'
 SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1200
 
+game_active = True
 scroll_sky = 0
 scroll_ground = 0
 snail_y = 310
 snail1_speed = 3
 snail2_speed = 2
+# Initialize movement variables
+moving_left = False
+moving_right = False
+is_midair = False
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT)) #width,height
@@ -20,7 +25,12 @@ test_font = pygame.font.Font(font_path, 50)
 
 sky_surface = pygame.image.load(image_path + 'sky.png').convert()
 ground_surface = pygame.image.load(image_path + 'ground.png').convert()
-text_surface = test_font.render('HELLO BIG BOYS', False, 'White')
+score_surf = test_font.render('HELLO BIG BOYS', False, 'White')
+end_surf = test_font.render('Game Over', False, 'White')
+end_continue_surf = test_font.render('Press SPACE to continue', False, 'White')
+end_exit_surf = test_font.render('Press ESCAPE to exit', False, 'White')
+
+score_rect = score_surf.get_rect(center = (SCREEN_WIDTH/2, SCREEN_HEIGHT-50))
 snail1_surface = pygame.image.load(image_path + 'snail/snail1.png').convert_alpha()
 snail1_rect = snail1_surface.get_rect(midbottom = (SCREEN_WIDTH, snail_y))
 
@@ -29,6 +39,7 @@ snail2_rect = snail2_surface.get_rect(midbottom = (SCREEN_WIDTH, snail_y))
 
 player_surface = pygame.image.load(image_path + 'player/player_walk_1.png').convert_alpha()
 player_rect = player_surface.get_rect(midbottom=(50, 310))
+player_gravity = 0
 
 sky_width = sky_surface.get_width()
 ground_width = ground_surface.get_width()
@@ -43,43 +54,93 @@ while True:
             pygame.quit()
             exit()
 
-    #draw background
-    for i in range(0, tiles):
-        screen.blit(sky_surface, (i * sky_width + scroll_sky, 0))
-    for i in range(0, tiles):
-        screen.blit(ground_surface, (i * ground_width + scroll_ground, 310))
+        # Jump if spacekey down
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and not is_midair:
+                player_gravity = -20
+                is_midair = True
 
-    #scroll background
-    scroll_sky -= 0.25
-    scroll_ground -= 1
+        # Check for key presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_a:
+                moving_left = True
 
-    #reset scroll
-    if abs(scroll_sky) > sky_width:
-        scroll_sky = 0
-    if abs(scroll_ground) > ground_width:
-        scroll_ground = 0
+        # Check for key releases
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_a:
+                moving_left = False
 
-    #draw snails
-    screen.blit(snail1_surface, snail1_rect)
-    screen.blit(snail2_surface, snail2_rect)
+    # Update the game state
+    if moving_right and not is_midair:
+        player_rect.x += 5
+    if moving_left and not is_midair:
+        player_rect.x -= 5
 
-    #draw player
-    screen.blit(player_surface, player_rect)
+    if player_rect.bottom >= 310:
+        is_midair = False
 
-    #draw text
-    screen.blit(text_surface, (300,50))
+    if game_active:
+        #draw background
+        for i in range(0, tiles):
+            screen.blit(sky_surface, (i * sky_width + scroll_sky, 0))
+        for i in range(0, tiles):
+            screen.blit(ground_surface, (i * ground_width + scroll_ground, 310))
 
-    #moves the rect
-    snail1_rect.x -= snail1_speed
-    snail2_rect.x -= snail2_speed
+        #scroll background
+        scroll_sky -= 0.25
+        scroll_ground -= 1
 
-    if snail1_rect.right <= 0:
-        snail1_rect.left = SCREEN_WIDTH
-        snail1_speed = random.randint(3, 8)
+        #reset scroll
+        if abs(scroll_sky) > sky_width:
+            scroll_sky = 0
+        if abs(scroll_ground) > ground_width:
+            scroll_ground = 0
 
-    if snail2_rect.right <= 0:
-        snail2_rect.left = SCREEN_WIDTH
-        snail2_speed = random.randint(3, 8)
+        #draw snails
+        screen.blit(snail1_surface, snail1_rect)
+        screen.blit(snail2_surface, snail2_rect)
+
+        #draw player
+        player_gravity += 1
+        player_rect.y += player_gravity
+        if player_rect.bottom >= 310:
+            player_rect.bottom = 310
+        screen.blit(player_surface, player_rect)
+
+        #draw text
+        screen.blit(score_surf, score_rect)
+
+        #moves the rect
+        snail1_rect.x -= snail1_speed
+        snail2_rect.x -= snail2_speed
+
+        if snail1_rect.right <= 0:
+            snail1_rect.left = SCREEN_WIDTH
+            snail1_speed = random.randint(3, 8)
+
+        if snail2_rect.right <= 0:
+            snail2_rect.left = SCREEN_WIDTH
+            snail2_speed = random.randint(3, 8)
+
+        if snail1_rect.colliderect(player_rect) or snail2_rect.colliderect(player_rect):
+            game_active = False
+    else:
+        #screen.fill('Black')
+        screen.blit(score_surf, score_rect)
+        screen.blit(end_surf, (400, 200))
+        screen.blit(end_continue_surf, (400, 230))
+        screen.blit(end_exit_surf, (400, 260))
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                exit()
+            elif event.key == pygame.K_SPACE:
+                game_active = True
 
     pygame.display.update()
     clock.tick(60) #should not run faster than 60 fps / max fps
---
+++ test.py
@@ -0,0 +1,2 @@
+p = 'Python'
+print('{} rules!'.format(p))
